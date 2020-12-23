5b32e89993d4d666b318a955e1b981f1073acbd9
Akash Singh <akash22675@gmail.com>
2020-12-23 22:02:12 +0530

    website deployment

---
+++ client/src/components/Society/Components/facultyList.js
@@ -0,0 +1,20 @@
+
+const facultyList = [
+    {
+        image: 'https://scontent.fdel1-1.fna.fbcdn.net/v/t31.0-8/20451656_568197123350883_5172117675625138101_o.jpg?_nc_cat=109&ccb=2&_nc_sid=09cbfe&_nc_ohc=g64UUDW3crkAX9pWRgQ&_nc_ht=scontent.fdel1-1.fna&oh=bef64de2d875234feca8ae69e92bfda4&oe=5FEED503',
+        description: 'ASHWAMEDH-The Dramatics Society of NSUT aims at shedding light upon the issues plaguing our society through street and stage plays with a twist of satire.ASHWAMEDH-The Dramatics Society of NSUT aims at shedding light upon the issues plaguing our society through street and stage plays with a twist of satire.ASHWAMEDH-The Dramatics Society of NSUT aims at shedding light upon the issues plaguing our society through street and stage plays with a twist of satire.'
+    }, 
+
+    {
+        image: 'https://d8it4huxumps7.cloudfront.net/uploads/images/opportunity/gallery/5e4c114981ae6_images__1_.png?d=600x600',
+        name: 'The Debating Society of NSUT',
+        description: 'The debating society of NSIT is a promoter of discussion and deliberation, and focuses on providing people a platform for shedding their inhibitions and learning the art of expressing. It is an open society which primarily means one doesn\'t have to be a member to take part in society\'s activities. No prerequisite is demanded from participants, just the will to get involved and speak.'
+    },
+    {
+        image: 'https://d8it4huxumps7.cloudfront.net/uploads/images/opportunity/gallery/5e4c114981ae6_images__1_.png?d=600x600',
+        name: 'The Debating Society of NSUT',
+        description: 'The debating society of NSIT is a promoter of discussion and deliberation, and focuses on providing people a platform for shedding their inhibitions and learning the art of expressing. It is an open society which primarily means one doesn\'t have to be a member to take part in society\'s activities. No prerequisite is demanded from participants, just the will to get involved and speak.'
+    }
+]
+
+export default facultyList
